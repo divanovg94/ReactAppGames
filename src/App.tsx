26e042b3142072349ee1f96155b6269b56fc49c2
divanovg94 <divanovg94@gmail.com>
2023-06-12 14:40:03 +0300

    Expense Tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
-import Alert from "./components/Alert";
+import { AiFillHeart } from "react-icons/ai";
+import ListGroup from "./components/ListGroup";
+import { BsFillCalendarFill } from "react-icons/bs";
+import Like from "./components/Like";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import { useState } from "react";
+import ExpenseFilter from "./expense-tracker/ExpenseFilter";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "ddsa", amount: 10, category: "Utilities" },
+    { id: 3, description: "asdas", amount: 10, category: "Utilities" },
+    { id: 4, description: "dasda", amount: 10, category: "Utilities" },
+  ]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+  const items = ["New York", "Los Angeles", "San Francisco"];
+
   return (
     <div>
-      <Alert>Hello World</Alert>
+      <Like onClick={() => console.log("clicked")} />
+      <ListGroup heading="Miami" items={items} onSelectItem={() => {}} />
+      <BsFillCalendarFill />
+      <div>
+        <div className="mb-3">
+          <ExpenseFilter
+            onSelectCategory={(category) => setSelectedCategory(category)}
+          />
+        </div>
+        <ExpenseList
+          expenses={visibleExpenses}
+          onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+        />
+      </div>
     </div>
   );
 }
